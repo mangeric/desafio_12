c67bdb7245f114242e47f5e69c66fa58c03b234d
mangeric <56059523+mangeric@users.noreply.github.com>
2021-10-11 16:42:55 -0300

    Add files via upload

---
+++ desafio_12/js/desafio_10.js
@@ -0,0 +1,121 @@
+const array_comidas=[];
+const carrito=[];
+const nombre=$('#nombre');
+const precio=document.getElementById('precio');
+const menu=document.getElementById('menu');
+const items=document.getElementById('items');
+const footer=document.getElementById('footer');
+const templeate_carrito=document.getElementById('template-carrito');
+const templeate_footer=document.getElementById('template-footer');
+const nuevo=$('#menu');
+
+class Comida{
+    constructor(id,nombre,precio,stock,cantidad){
+        this.id=id;
+        this.nombre=nombre;
+        this.precio=precio;
+        this.stock=stock;
+        this.cantidad=cantidad||0;
+    } 
+    aumentarCantidad(){
+        this.cantidad++
+    }
+}
+const milanesa=new Comida(1,'milanesa',420,5);
+const milanesa_napo= new Comida(2,"milanesa_napo",490,2);
+const canelon= new Comida(3,'canelon',420,10);
+const albondiga= new Comida(4,'albondiga',440,5);
+
+array_comidas.push(milanesa,milanesa_napo,canelon,albondiga);
+const clase= $('#menu');
+for(const comida of array_comidas){
+    const contenedor = document.createElement('div')
+    contenedor.className='card'
+    contenedor.innerHTML=`
+                        <img src="img/img_${comida.id}.jpg" class="card-img-top" alt=${comida.nombre}>
+                        <div class="card-body">
+                            <h5 class="card-title">${comida.nombre}</h5>
+                            <p class="card-text">precio: ${comida.precio}</p>
+                            <button id=${comida.id} class="btn btn-primary comprar btnMostrar">Comprar</button>
+                        </div>`
+    clase.append(contenedor)
+    
+    $(`#${comida.id}`).click(() => comprar(comida))
+    $(`#${comida.id}`).click(() => {
+        $(`#${comida.id}`).addClass("animate__animated animate__fadeInLeft")
+        setTimeout(()=>{$(`#${comida.id}`).removeClass("animate__animated animate__fadeInLeft")},1000)
+        })
+
+}
+
+function comprar(producto){
+    console.log(producto)
+    let compra = carrito.find(el=> el.nombre=== producto.nombre)
+    if(compra){
+        if(compra.cantidad < producto.stock){
+            compra.aumentarCantidad();
+            console.log("se aumento la cantidad")
+        }else{
+            alert('no hay mas stock')
+        }
+    }else{
+        carrito.push(producto);
+        producto.aumentarCantidad();
+    }
+    let total = 0;
+    for(let i=0; i<carrito.length;i++){
+        total += carrito[i].cantidad;
+    }
+    const contador = document.getElementById('contador');
+    contador.innerHTML = total;
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+    pintar_carrito(carrito);
+}
+
+function cargarLocalStorage(){
+    let carro = JSON.parse(localStorage.getItem('carrito'))
+    if(carro){
+        for(let i = 0; i < carro.length; i++){
+            carrito.push(new Comida(carro[i].id, carro[i].nombre, carro[i].precio, carro[i].stock, carro[i].cantidad))
+        }
+    }
+    console.log(carrito)
+    pintar_carrito(carrito);
+   
+}
+
+
+
+function pintar_carrito(carrito){
+    $("#items").empty();
+    for(let i=0; i<carrito.length;i++){
+        $("#items").append(`<div id="template-carrito">
+            <tr>
+            <th scope="row"></th>
+            <td>${carrito[i].nombre}</td>
+            <td>${carrito[i].cantidad}</td>
+            <td>
+                <button id="${carrito[i].id}" class="btn btn-info btn-sm">
+                    +
+                </button>
+                <button id="${carrito[i].id}" class="btn btn-danger btn-sm">
+                    -
+                </button>
+            </td>
+            <td>$ <span>${carrito[i].precio}</span></td>
+            </tr>
+            </div>`
+        );
+    }
+}
+
+
+/*
+const add_carrito=e=>{
+    console.log(e.target);
+    if(e.target.classList.contains('btn-primary')){}
+}
+menu.addEventListener('click',e =>{
+    add_carrito(e);
+})
+*/
